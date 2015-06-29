9f196897856cfb630e8cfdc6d5e9d93880e6ceec
lHydra <skywalqqer@yandex.ru>
2015-06-29 16:31:45 +0600

    Some changes with setting responders gem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  respond_to :html, :xml, :json
 
   # GET /posts
   # GET /posts.json
